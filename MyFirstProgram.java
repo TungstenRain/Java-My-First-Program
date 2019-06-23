a4f5e304bbcf2667b04c6336bc81910bf1e85fec
Frank Olson <frank.olson@gmail.com>
2019-06-23 03:43:09 -0600

    Add files via upload

---
+++ MyFirstProgram.java
@@ -0,0 +1,13 @@
+// This is the first project for Java I
+// Purpose: To print "Hello World" to the screen
+// Author: Frank Olson
+// Date: 10-18-2017
+
+public class MyFirstProgram
+{
+   public static void main(String[] args)
+   {
+      //Print Hello World
+      System.out.println("Hello World");
+   }
+}
